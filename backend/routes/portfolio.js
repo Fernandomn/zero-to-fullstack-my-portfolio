d4c5dce73602d8034ed2c0791d4a95f4607514ed
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-10-17 18:43:03 -0300

    Creating Delete. Updating Patch endpoint

--- backend/routes/portfolio.js
+++ backend/routes/portfolio.js
@@ -43,12 +43,31 @@ router.patch("/:slug", async (req, res) => {
       { title: req.body.title, description: req.body.description }
     );
 
-    res.json({ success: true, data: updatedPortfolio });
+    res.json({
+      success: true,
+      updatedCount: updatedPortfolio.modifiedCount,
+      data: updatedPortfolio,
+    });
   } catch (err) {
     res.json({ success: false, message: err });
   }
 });
 
 // DELETE
+router.delete("/:slug", async (req, res) => {
+  try {
+    const deletedPortfolio = await Portfolio.deleteOne({
+      slug: req.params.slug,
+    });
+
+    res.json({
+      success: true,
+      deletedCount: deletedPortfolio.deletedCount,
+      data: deletedPortfolio,
+    });
+  } catch (err) {
+    res.json({ success: false, message: err });
+  }
+});
 
 module.exports = router;
