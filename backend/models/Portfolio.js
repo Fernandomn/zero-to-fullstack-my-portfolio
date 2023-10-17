f7cdccfa938ab4965ef086828a403184e54d5f48
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-10-17 17:46:58 -0300

    applying slug in Portfolio routes

--- backend/models/Portfolio.js
+++ backend/models/Portfolio.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
-
+const slug = require("slug");
 const { Schema } = mongoose;
 
 const portfolioSchema = new Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+    default: function(){ return slug(this.title)},
+  },
   description: { type: String, required: true },
   createdAt: { type: Date, default: Date.now() },
 });
--- backend/routes/portfolio.js
+++ backend/routes/portfolio.js
@@ -1,17 +1,7 @@
 const router = require("express").Router();
 const Portfolio = require("./../models/Portfolio");
 
-// /portfolio
-router.get("/", async (req, res) => {
-  try {
-    const portfolio = await Portfolio.find();
-
-    res.json({ success: true, data: portfolio });
-  } catch (err) {
-    res.json({ success: false, message: err });
-  }
-});
-
+// CREATE
 router.post("/", async (req, res) => {
   const portfolio = new Portfolio({
     title: req.body.title,
@@ -25,5 +15,28 @@ router.post("/", async (req, res) => {
     res.json({ success: false, message: err });
   }
 });
+
+// READ
+router.get("/", async (req, res) => {
+  try {
+    const portfolio = await Portfolio.find();
+
+    res.json({ success: true, data: portfolio });
+  } catch (err) {
+    res.json({ success: false, message: err });
+  }
+});
+router.get("/:slug", async (req, res) => {
+  try {
+    const portfolio = await Portfolio.findOne({ slug: req.params.slug });
+    res.json({ success: true, data: portfolio });
+  } catch (err) {
+    res.json({ success: false, message: err });
+  }
+});
+
+// UPDATE
+
+// DELETE
 
 module.exports = router;
