93b6a0d789b5e2d3295c290ad079077e2db6a0ae
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-10-19 19:13:07 -0300

    first developments in react

---
+++ frontend/src/components/AboutSection.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AboutSection = () => {
+  return (
+    <>
+      <h2>Fernando Medeiros</h2>
+      <h2>Software Engineer</h2>
+
+      <p>
+        I'm a 32 YO brazillian developer, that is pivoting from front-end to
+        full-stack development
+      </p>
+
+      <button>Contact Me</button>
+
+      <img src="https://picsum.photos/300/200" alt="placeholder" />
+    </>
+  );
+};
+
+export default AboutSection;
---
+++ frontend/src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AboutSection from "../components/AboutSection";
+
+const AboutUs = () => {
+  return (
+    <>
+      <div>About us</div>
+      <AboutSection />
+    </>
+  );
+};
+
+export default AboutUs;
