b0488533983467d2450be2414653ac238604d1a8
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-11-16 17:23:36 -0300

    Adding configs to deploy

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ const api = require("./backend/routes");
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.json({ success: true });
-});
-
 app.use("/api", api);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT;
 app.listen(PORT);
