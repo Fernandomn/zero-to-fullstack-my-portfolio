b143a0aeabbb7a718c9845997474d37fec871f05
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-11-16 10:29:24 -0300

    Creating contact form

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -2,10 +2,26 @@ import React from "react";
 
 const Contact = () => {
   const socialList = [
-    { id: 1, icon: "icon", title: "GitHub", url: "" },
-    { id: 2, icon: "icon", title: "LinkedIn", url: "" },
-    { id: 3, icon: "icon", title: "Youtube", url: "" },
+    {
+      id: 1,
+      icon: "icon",
+      title: "GitHub",
+      url: "https://github.com/Fernandomn",
+    },
+    {
+      id: 2,
+      icon: "icon",
+      title: "LinkedIn",
+      url: "https://www.linkedin.com/in/fernando-medeiros-do-nascimento/",
+    },
+    {
+      id: 3,
+      icon: "icon",
+      title: "Youtube",
+      url: "https://www.youtube.com/channel/UCqjeM9Cmz1rHru967lsulmw",
+    },
     { id: 4, icon: "icon", title: "Udemy", url: "" },
+    { id: 4, icon: "icon", title: "Telegram", url: "https://t.me/ferna_mn" },
   ];
   const renderSocialList = () => {
     return socialList.map((social) => (
@@ -20,7 +36,14 @@ const Contact = () => {
     <div>
       <div>Get in touch</div>
       <div id="areas">
-        <div id="form"></div>
+        <div id="form">
+          <form id="contact-form" method="post">
+            Name: <input type="text" />
+            Email: <input type="email" />
+            Message: <textarea rows="5" />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
         <div id="networks">
           <div id="social">{renderSocialList()}</div>
         </div>
